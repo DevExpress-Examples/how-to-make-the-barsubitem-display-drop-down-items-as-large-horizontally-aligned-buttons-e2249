ae4ce574dd4e444a07a674e3171ad38ec4c11238
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXRibbonSample/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Ribbon;
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Core;
+
+namespace Q421413 {
+	public partial class MainWindow: DXWindow {
+		static MainWindow() {
+			BarItemLinkControlCreator.Default.RegisterObject(typeof(MyBarCheckItemLink),
+			typeof(MyBarCheckItemLinkControl),
+			link => new MyBarCheckItemLinkControl((MyBarCheckItemLink)link));
+			BarItemLinkControlCreator.Default.RegisterObject(typeof(MyBarButtonItemLink),
+			typeof(MyBarButtonItemLinkControl),
+			link => new MyBarButtonItemLinkControl((MyBarButtonItemLink)link));
+		}
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
+	public class MyBarCheckItem: BarCheckItem {
+		protected override Type GetLinkType() {
+			return typeof(MyBarCheckItemLink);
+		}
+	}
+	public class MyBarCheckItemLink: BarCheckItemLink {
+		protected override BarItemLinkControlBase CreateBarItemLinkControl() {
+			return new MyBarCheckItemLinkControl(this);
+		}
+	}
+	public class MyBarCheckItemLinkControl: BarCheckItemLinkControl {
+		public MyBarCheckItemLinkControl() { }
+		public MyBarCheckItemLinkControl(BarCheckItemLink link) : base(link) { }
+		protected override void UpdateActualGlyphAlignmentInRibbon() {
+			//switch (CurrentRibbonStyle) {
+			//    case RibbonItemStyles.Default:
+			//        break;
+			//    case RibbonItemStyles.Large:
+			//        ActualGlyphAlignment = Dock.Top;
+			//        break;
+			//    default:
+			//        ActualGlyphAlignment = Dock.Left;
+			//        break;
+			//}
+			ActualGlyphAlignment = Dock.Top;
+		}
+		protected override void UpdateActualGlyphAlignmentInBars() {
+			//ActualGlyphAlignment = GetGlyphAlignment();
+			ActualGlyphAlignment = Dock.Top;
+		}
+		//Dock GetGlyphAlignment() {
+		//    if (IsLinkControlInMenu) return Dock.Left;
+		//    if (Link != null && Link.UserGlyphAlignment.HasValue)
+		//        return Link.UserGlyphAlignment.Value;
+		//    if (Item != null)
+		//        return Item.GlyphAlignment;
+		//    return Dock.Left;
+		//}
+	}
+
+	public class MyBarButtonItem: BarButtonItem {
+		protected override Type GetLinkType() {
+			return typeof(MyBarButtonItemLink);
+		}
+	}
+	public class MyBarButtonItemLink: BarButtonItemLink {
+		protected override BarItemLinkControlBase CreateBarItemLinkControl() {
+			return new MyBarButtonItemLinkControl(this);
+		}
+	}
+	public class MyBarButtonItemLinkControl: BarButtonItemLinkControl {
+		public MyBarButtonItemLinkControl() { }
+		public MyBarButtonItemLinkControl(MyBarButtonItemLink link) : base(link) { }
+		protected override void UpdateActualGlyphAlignmentInRibbon() {
+			ActualGlyphAlignment = Dock.Top;
+		}
+		protected override void UpdateActualGlyphAlignmentInBars() {
+			ActualGlyphAlignment = Dock.Top;
+		}
+	}
+}
---
+++ VB/DXRibbonSample/MainWindow.xaml.vb
@@ -0,0 +1,102 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Ribbon
+Imports DevExpress.Xpf.Bars
+Imports DevExpress.Xpf.Core
+
+Namespace Q421413
+	Partial Public Class MainWindow
+		Inherits DXWindow
+		Shared Sub New()
+			BarItemLinkControlCreator.Default.RegisterObject(GetType(MyBarCheckItemLink), GetType(MyBarCheckItemLinkControl), Function(link) New MyBarCheckItemLinkControl(CType(link, MyBarCheckItemLink)))
+			BarItemLinkControlCreator.Default.RegisterObject(GetType(MyBarButtonItemLink), GetType(MyBarButtonItemLinkControl), Function(link) New MyBarButtonItemLinkControl(CType(link, MyBarButtonItemLink)))
+		End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+	Public Class MyBarCheckItem
+		Inherits BarCheckItem
+		Protected Overrides Function GetLinkType() As Type
+			Return GetType(MyBarCheckItemLink)
+		End Function
+	End Class
+	Public Class MyBarCheckItemLink
+		Inherits BarCheckItemLink
+		Protected Overrides Function CreateBarItemLinkControl() As BarItemLinkControlBase
+			Return New MyBarCheckItemLinkControl(Me)
+		End Function
+	End Class
+	Public Class MyBarCheckItemLinkControl
+		Inherits BarCheckItemLinkControl
+		Public Sub New()
+		End Sub
+		Public Sub New(ByVal link As BarCheckItemLink)
+			MyBase.New(link)
+		End Sub
+		Protected Overrides Sub UpdateActualGlyphAlignmentInRibbon()
+			'switch (CurrentRibbonStyle) {
+			'    case RibbonItemStyles.Default:
+			'        break;
+			'    case RibbonItemStyles.Large:
+			'        ActualGlyphAlignment = Dock.Top;
+			'        break;
+			'    default:
+			'        ActualGlyphAlignment = Dock.Left;
+			'        break;
+			'}
+			ActualGlyphAlignment = Dock.Top
+		End Sub
+		Protected Overrides Sub UpdateActualGlyphAlignmentInBars()
+			'ActualGlyphAlignment = GetGlyphAlignment();
+			ActualGlyphAlignment = Dock.Top
+		End Sub
+		'Dock GetGlyphAlignment() {
+		'    if (IsLinkControlInMenu) return Dock.Left;
+		'    if (Link != null && Link.UserGlyphAlignment.HasValue)
+		'        return Link.UserGlyphAlignment.Value;
+		'    if (Item != null)
+		'        return Item.GlyphAlignment;
+		'    return Dock.Left;
+		'}
+	End Class
+
+	Public Class MyBarButtonItem
+		Inherits BarButtonItem
+		Protected Overrides Function GetLinkType() As Type
+			Return GetType(MyBarButtonItemLink)
+		End Function
+	End Class
+	Public Class MyBarButtonItemLink
+		Inherits BarButtonItemLink
+		Protected Overrides Function CreateBarItemLinkControl() As BarItemLinkControlBase
+			Return New MyBarButtonItemLinkControl(Me)
+		End Function
+	End Class
+	Public Class MyBarButtonItemLinkControl
+		Inherits BarButtonItemLinkControl
+		Public Sub New()
+		End Sub
+		Public Sub New(ByVal link As MyBarButtonItemLink)
+			MyBase.New(link)
+		End Sub
+		Protected Overrides Sub UpdateActualGlyphAlignmentInRibbon()
+			ActualGlyphAlignment = Dock.Top
+		End Sub
+		Protected Overrides Sub UpdateActualGlyphAlignmentInBars()
+			ActualGlyphAlignment = Dock.Top
+		End Sub
+	End Class
+End Namespace
